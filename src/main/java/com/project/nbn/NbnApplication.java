e39ac26472cf2ddf90e3952139a75c0d980e256e
NgocNB20 <95111259+NgocNB20@users.noreply.github.com>
2024-03-12 23:19:28 +0700

    k

---
+++ src/main/java/com/project/nbn/NbnApplication.java
@@ -0,0 +1,13 @@
+package com.project.nbn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbnApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/nbn/constant/EnumType.java
@@ -0,0 +1,15 @@
+package com.project.nbn.constant;
+
+public interface EnumType {
+    /**
+     *
+     * @return string name Enum;
+     */
+    String getLabel();
+
+    /**
+     *
+     * @return value Enum;
+     */
+    String getValue();
+}
